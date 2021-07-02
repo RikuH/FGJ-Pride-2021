4cb31c3ea5a8c9e456dec07c83f56d404340c0ab
RikuH <45202322+RikuH@users.noreply.github.com>
2021-07-02 23:12:22 +0300

    JeesBox

---
+++ Assets/Scripts/CarScript.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Axel
+{
+    Front,
+    Rear
+}
+[Serializable]
+public struct Wheel
+{
+    public GameObject Model;
+    public WheelCollider collider;
+    public Axel axel;
+}
+
+public class CarScript : MonoBehaviour
+{
+
+    public bool isRiding = false;
+
+    [SerializeField] float maxAcceleration = 20.0f;
+    [SerializeField] float turnSensitivity = 1.0f;
+    [SerializeField] float maxSteerAngle = 45.0f;
+    [SerializeField] List<Wheel> Wheels;
+
+    private float inputX, inputY;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isRiding)
+            return;
+
+        GetInputs();
+    }
+    private void LateUpdate()
+    {
+        if (!isRiding)
+            return;
+
+        Move();
+    }
+    void GetInputs()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        inputY = Input.GetAxis("Vertical");
+    }
+    private void Move()
+    {
+        foreach(var wheel in Wheels)
+        {
+            wheel.collider.motorTorque = inputY * maxAcceleration * 500 * Time.deltaTime;
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,20 @@ using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
+    //Get in the car variables :D
     [SerializeField] GameObject DriverDoor;
+    [SerializeField] GameObject Car;
     [SerializeField] float AbleToGetInCarDistance;
+    bool isGoingToDrive = false;
+
+    //Free moving variables
+    public CharacterController controller;
+    public float speed = 6f;
+    public float turnSmoothTime = 0f;
+    public Transform cam;
+    float turnSmoothVelocity;
 
     NavMeshAgent agent;
-    bool isGoingToDrive = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +29,20 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
         float distance = Vector3.Distance(gameObject.transform.position, DriverDoor.transform.position);
         if (distance < AbleToGetInCarDistance)
         {
@@ -33,8 +56,14 @@ public class Player : MonoBehaviour
                 if(agent.remainingDistance < 0.1f)
                 {
                     Debug.Log("Nyt ajaa");
+                    Car.GetComponent<CarScript>().isRiding = true;
                 }
             }
+        }
+        else
+        {
+            isGoingToDrive = false;
+            agent.ResetPath();
         }
     }
 }
